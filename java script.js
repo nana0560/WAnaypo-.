ecfdf023587a80accb1a513a8a33fb68cdaa8323
nana0560 <126900493+nana0560@users.noreply.github.com>
2023-11-22 16:30:24 +0000

    Add files via upload

---
+++ java script.js
@@ -0,0 +1,5 @@
+<section id="hero">
+    <h1 id="hero-heading">Welcome to the Computer Science Department</h1>
+    <p>Explore our programs, research, and events.</p>
+    <button id="colorButton">Change Heading Color</button>
+</section>
